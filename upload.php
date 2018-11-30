0817c700078ad45f6d9aca9298f908d3aa6298e7
Vaibhav Tapadiya <vaibhav.tapadiya@weboniselab.com>
2018-11-30 19:35:57 +0530

    Changes on Firday

--- upload.php
+++ upload.php
@@ -1,29 +1,32 @@
 <?php
       require_once 'index2.php';
       require_once 'DocxConversion.php';
-
+    
+      $file_info = finfo_open(FILEINFO_MIME_TYPE);
       $errors= array();
       $title=$_POST['title'];
       $image_file_name = $_FILES['image']['name'];
       $image_file_size =$_FILES['image']['size'];
       $image_file_tmp =$_FILES['image']['tmp_name'];
-      $image_file_type=$_FILES['image']['type'];
-      $image_file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      $image_file_type=finfo_file($file_info,$_FILES['image']['tmp_name']);
 
+    
       $doc_file_name = $_FILES['document']['name'];
       $doc_file_size =$_FILES['document']['size'];
       $doc_file_tmp =$_FILES['document']['tmp_name'];
-      $doc_file_type=$_FILES['document']['type'];
-      $doc_file_ext=strtolower(end(explode('.',$_FILES['document']['name'])));
+      $doc_file_type=finfo_file($file_info,$_FILES['document']['tmp_name']);
+      $doc_file_type=explode(".",$doc_file_type)[3];
+      
 
-      $image_extensions= array("jpeg","jpg","png");
-      $doc_extensions= "docx";
 
-      if(in_array($image_file_ext,$image_extensions)=== false){
-        $errors=$image_file_ext." extension not allowed, please choose a .JPEG or .PNG file.";
+      $image_extensions= array("image/jpeg","image/jpg","image/png");
+      $doc_extensions= "document";
+
+      if(in_array($image_file_type,$image_extensions)=== false){
+        $errors=" extension not allowed, please choose a .JPEG or .PNG file.";
       }
-      if(!($doc_file_ext===$doc_extensions)){
-        $errors=$doc_file_ext." extension not allowed, please choose a .doc file.";
+      if(!($doc_file_type===$doc_extensions)){
+        $errors=" extension not allowed, please choose a .doc file.";
       }
       if($image_file_size > 2097152){
          $errors="Image File size must Not be greater than 2 MB";
@@ -36,7 +39,10 @@
       if(empty($errors)){
 
          $data="Together we can change the world, just one random act of kindness at a time.";
-         addtext($doc_path,$data);
+
+         $fp = fopen($doc_file_name, 'a+');
+         fwrite($fp, $data);
+         fclose($fp);
          move_uploaded_file($image_file_tmp,$image_path);
          move_uploaded_file($doc_file_tmp,$doc_path);
          echo "Files Uploaded Successfully";
@@ -63,7 +69,6 @@
       }
       function insertData($title,$image_path,$doc_path)
       {
-          // $conn=connect();
           $servername = "localhost";
           $username = "root";
           $password = "";
@@ -77,33 +82,10 @@
                 $stmt->execute();
 
                 echo "New records created successfully";
-
-                //$stmt->close();
-                //$conn->close();
           }
           catch(PDOException $e)
           {
               echo "Connection failed: " . $e->getMessage();
           }
-      }
-      /*function read_file_docx($filename){
-          $striped_content = '';
-          $content = '';
-            if(!$filename || !file_exists($filename)) return false;
-                $zip = zip_open($filename);
-            if (!$zip || is_numeric($zip)) return false;
-
-            while ($zip_entry = zip_read($zip)) {
-
-              if (zip_entry_open($zip, $zip_entry) == FALSE) continue;
-              if (zip_entry_name($zip_entry) != "word/document.xml") continue;
-                  $content .= zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
-              zip_entry_close($zip_entry);
-            }
-          zip_close($zip);
-          $content = str_replace('</w:r></w:p></w:tc><w:tc>', " ", $content);
-          $content = str_replace('</w:r></w:p>', "\r\n", $content);
-          $striped_content = strip_tags($content);
-          return $striped_content;
-      }*/
+        }
 ?>
